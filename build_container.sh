c38a1b20a42daf5944ed8d1ff4a56d7c6d98bf13
Maximilian Klein <dasmaxl@gmail.com>
2015-12-20 18:19:48 +0100

    (chore) Introduce CACHE option for build script.

--- build_container.sh
+++ build_container.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-: "${LOCAL_REGISTRY?Need to set LOCAL_REGISTRY for publishing}"
-
 echo "Using node version `node --version`"
 
 VERSION=`less tutor-student/package.json | jq '.version' | tr -d '"'`
@@ -12,7 +10,18 @@ cd tutor-student && npm install && bower install && NODE_ENV=production gulp && 
 
 echo "Building container"
 
-#docker build --no-cache -t welfenlab-student:$VERSION . && \
-docker build -t welfenlab-student:$VERSION . && \
-docker tag -f welfenlab-student:$VERSION $LOCAL_REGISTRY/welfenlab-student:$VERSION && \
-docker push $LOCAL_REGISTRY/welfenlab-student:$VERSION
+if [ -z "$CACHE" ]; then
+  # the default behaviour uses NO CACHE! to avoid caching problems at any costs. Using gulp might change something
+	# in the project but docker does not always notice that
+	docker build --no-cache -t welfenlab-student:$VERSION . && \
+else
+  # if CACHE is set explicitly build using cache
+	docker build -t welfenlab-student:$VERSION . && \
+fi
+if [ -z "$LOCAL_REGISTRY" ]; then
+  echo "No local registry configured. Won't push into registry."
+else
+	echo "Registry is configured, updating registry entry in $LOCAL_REGISTRY."
+	docker tag -f welfenlab-student:$VERSION $LOCAL_REGISTRY/welfenlab-student:$VERSION && \
+	docker push $LOCAL_REGISTRY/welfenlab-student:$VERSION
+fi
